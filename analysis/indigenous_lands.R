e945e5428bd17dd631cae32d6177b0d41e76be7d
hugotseixas <seixas.hugo@protonmail.com>
2023-03-31 15:36:24 -0300

    Add indigenous lands script.

---
+++ analysis/indigenous_lands.R
@@ -0,0 +1,34 @@
+# HEADER ----------------------------------------------------------------------
+#
+# Title:          Process Indigenous Lands Data
+# Description:    This process Indigenous Lands data to fit the base grid
+#
+# Notes:
+#
+# LIBRARIES -------------------------------------------------------------------
+#
+library(conflicted)
+library(arrow)
+library(tica)
+#
+# CONFLICTS -------------------------------------------------------------------
+#
+
+conflicts_prefer(dplyr::filter)
+
+#
+# OPTIONS ---------------------------------------------------------------------
+#
+
+#
+# PROCESS AND SAVE UC DATA ----------------------------------------------------
+
+indigenous_lands_grid <-
+  process_indigenous_lands()
+
+# Save grid polygons as FlatGeobuf file
+write_parquet(
+  x = indigenous_lands_grid,
+  sink = "data/indigenous_lands.parquet",
+  version = "latest"
+)
