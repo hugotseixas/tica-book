98757ca19384aaed559cf398ef92bd1f247af4fa
hugotseixas <seixas.hugo@protonmail.com>
2023-04-06 08:01:46 -0300

    Overall change on how to create vizualizations.

--- analysis/eda_deforestation.R
+++ analysis/eda_deforestation.R
@@ -1,7 +1,7 @@
 # HEADER ----------------------------------------------------------------------
 #
 # Title:          EDA for deforestation variable
-# Description:    This script explores deforestation data from PRODES
+# Description:    This script explores deforestation data from Mapbiomas
 #
 # Notes:
 #
@@ -13,7 +13,9 @@ library(arrow)
 library(glue)
 library(scales)
 library(scico)
+library(cowplot)
 library(tidyverse)
+library(tica)
 #
 # CONFLICTS -------------------------------------------------------------------
 #
@@ -24,238 +26,231 @@ conflicts_prefer(dplyr::filter)
 # OPTIONS ---------------------------------------------------------------------
 #
 
+conflicts_prefer(dplyr::filter)
+
 #
 # LOAD DATA -------------------------------------------------------------------
 
 aoi <- read_sf("data/external/aoi/aoi.fgb")
 
-defo <-
-  read_parquet("data/deforestation.parquet") %>%
-  summarise(
-    deforestation_area = sum(deforestation_area),
-    .by = c("year", "cell_id")
-  )
+defo <- read_parquet("data/deforestation.parquet")
 
 base_grid <- read_sf("data/base_grid.fgb")
 
+# PROCESS DATA ---------------------------------------------------------------
+
+defo <- defo |>
+  left_join(
+    base_grid,
+    by = join_by(cell_id)
+  ) |>
+  mutate(deforestation_area = deforestation_area * 0.001)
+
 # EXPLORE DATA ----------------------------------------------------------------
 
-## Create Histogram Plot ----
+## Create Histogram Visualization ----
+viz_list <-
+  map(
+    c(3, 1),
+    \(region) {
 
-# Create plot
-(
-  hist_plot <- defo %>%
-    drop_na() %>%
-    mutate(deforestation_area = deforestation_area * 0.0001) %>%
-    ggplot() +
-    geom_histogram(
-      aes(x = deforestation_area, y = after_stat(count)),
-      color = "#000000",
-      fill = "#e8e8e8",
-      bins = 50
-    ) +
-    scale_x_log10(
-      breaks = log_breaks(6),
-      labels = label_number(accuracy = 1),
-      expand = c(0, 0)
-    ) +
-    labs(
-      title = "Deforestation Histogram",
-      x = "Area (ha)"
-    ) +
-    theme_minimal() +
-    theme(
-      text = element_text(size = 18),
-      axis.text.y = element_blank(),
-      axis.title.y = element_blank(),
-      panel.grid.major.y = element_blank(),
-      panel.grid.minor.y = element_blank()
-    )
-)
+      viz <- defo |>
+        filter(region_code == region) |>
+        summarise(
+          deforestation_area = sum(deforestation_area),
+          .by = c("cell_id", "region_name", "year")
+        ) |>
+        eda_histogram(
+          variable = deforestation_area,
+          n_bins = 50,
+          scale_transform = "log",
+          xlim = c(70, 600000)
+        ) +
+        facet_wrap(facets = vars(region_name)) +
+        theme(
+          strip.text = element_text()
+        )
 
-## Create Cumulative Plot ----
+      if (region == 1) {
 
-# Calculate some percentiles
-quantile_table <- defo %>%
-  drop_na() %>%
-  #mutate(deforestation_area = deforestation_area * 0.0001) %>%
-  summarise(
-    #q01 = quantile(deforestation_area, 0.01),
-    q05 = quantile(deforestation_area, 0.05),
-    q25 = quantile(deforestation_area, 0.25),
-    q50 = quantile(deforestation_area, 0.5),
-    q75 = quantile(deforestation_area, 0.75),
-    q95 = quantile(deforestation_area, 0.95),
-    q99 = quantile(deforestation_area, 0.99),
-    q100 = quantile(deforestation_area, 1)
+        viz <- viz +
+          labs(x = "Deforestation Area (ha)") +
+          theme(
+            axis.title.x = element_text(),
+            axis.text.x = element_text(size = 10)
+          )
+
+      }
+
+      return(viz)
+
+    }
   )
 
-# Get cumulative sum for each percentile
-cumsum_values <- defo %>%
-  drop_na() %>%
-  #mutate(deforestation_area = deforestation_area * 0.0001) %>%
-  arrange(deforestation_area) %>%
-  mutate(
-    cumulative = cumsum(
-      deforestation_area/sum(deforestation_area)
-    )
-  ) %>%
-  inner_join(
-    quantile_table %>%
-      pivot_longer(everything()),
-    by = join_by(closest(deforestation_area >= value))
-  ) %>%
-  mutate(dif = deforestation_area - value) %>%
-  slice_min(order_by = dif, by = name) %>%
-  mutate(
-    cumulative = round(cumulative, digits = 2),
-    deforestation_area = round(deforestation_area),
-    quant = as.numeric(str_remove(name, "q"))
-  ) %>%
-  distinct(cumulative, deforestation_area, quant)
+title <- ggdraw() +
+  draw_label(
+    "Deforestation Histogram",
+    fontface = 'bold',
+    x = 0,
+    hjust = 0
+  ) +
+  theme(
+    plot.margin = margin(0, 0, 0, 0)
+  )
 
-# Create plot
-(
-  cumsum_plot <- defo %>%
-    drop_na() %>%
-    #mutate(deforestation_area = deforestation_area * 0.0001) %>%
-    arrange(deforestation_area) %>%
-    mutate(
-      cumulative = cumsum(deforestation_area/sum(deforestation_area)),
-      quant = case_when(
-        deforestation_area <= quantile_table[[1]] ~ "a",
-        deforestation_area <= quantile_table[[2]] ~ "b",
-        deforestation_area <= quantile_table[[3]] ~ "c",
-        deforestation_area <= quantile_table[[4]] ~ "d",
-        deforestation_area <= quantile_table[[5]] ~ "e",
-        deforestation_area <= quantile_table[[6]] ~ "f",
-        deforestation_area <= quantile_table[[7]] ~ "g"
-      )
-    ) %>%
-    ggplot(aes(x = deforestation_area, y = cumulative)) +
-    geom_ribbon(
-      aes(
-        ymin = 0,
-        ymax = cumulative,
-        fill = quant
-      ),
-      alpha = 0.7
-    ) +
-    geom_step(
-      linewidth = 0.5
-    ) +
-    annotate(
-      geom = "segment",
-      x = cumsum_values$deforestation_area,
-      xend = cumsum_values$deforestation_area,
-      y = 0,
-      yend = cumsum_values$cumulative,
-      linetype = 2
-    ) +
-    geom_label(
-      data = cumsum_values,
-      aes(label = glue("{quant}th")),
-      alpha = 0.9,
-      size = 3
-    ) +
-    scale_x_log10(
-      breaks = c(cumsum_values$deforestation_area),
-      labels = label_number(accuracy = 1, scale = 0.0001),
-      guide = guide_axis(angle = 55)
-    ) +
-    scale_y_continuous(
-      labels = label_percent(),
-      breaks = c(cumsum_values$cumulative, 1),
-      guide = guide_axis(check.overlap = TRUE)
-    ) +
-    scale_fill_scico_d(palette = "bilbao") +
-    labs(
-      title = "Cumulative Deforestation Area",
-      x = "Area (ha)",
-      y = "Cumulative Deforestation Percentage"
-    ) +
-    theme_minimal() +
-    theme(
-      legend.position = "",
-      text = element_text(size = 12),
-      panel.grid.minor = element_blank(),
-      panel.grid.major.x = element_blank()
-    )
+viz_hist <-
+  plot_grid(
+    title,
+    plotlist =  viz_list,
+    ncol = 1,
+    scale = 0.9,
+    rel_heights = c(0.1, 1, 1)
+  )
+
+save(viz_hist, file = "./figs/deforestation_hist.rdata")
+
+## Create Cumulative Visualization ----
+viz_list <-
+  map(
+    c(3, 1),
+    \(region) {
+
+      viz <- defo |>
+        filter(region_code == region) |>
+        summarise(
+          deforestation_area = sum(deforestation_area),
+          .by = c("cell_id", "region_name", "year")
+        ) |>
+        eda_cumulative_distribution(
+          variable = deforestation_area,
+          quantiles_list = c(0.005, 0.25, 0.5, 0.75, 0.95, 0.99, 1),
+          scale_transform = "log"
+        ) +
+        facet_wrap(facets = vars(region_name)) +
+        theme(
+          strip.text = element_text(),
+          axis.text = element_text(size = 10),
+          panel.grid.major.y = element_line(
+            color = "#999999",
+            linewidth = 0.1
+          ),
+          plot.margin = unit(c(-0.3, 0, 0, 0), "cm")
+        )
+
+      return(viz)
+
+    }
+  )
+
+title <- ggdraw() +
+  draw_label(
+    "Cumulative Percentage of Deforestation",
+    fontface = 'bold',
+    x = 0,
+    hjust = 0
+  ) +
+  theme(
+    plot.margin = margin(0, 0, 8, 0)
+  )
+
+viz_cumulative <-
+  plot_grid(
+    title,
+    plotlist =  viz_list,
+    ncol = 1,
+    rel_heights = c(0.1, 1, 1),
+    scale = 0.9
+  ) +
+  draw_label(
+    "Cumulative Deforestation Percentage (%)",
+    x = 0, y = 0.5,
+    vjust = 1.5,
+    angle = 90,
+    size = 13
+  ) +
+  draw_label(
+    "Deforestation Area (ha)",
+    x = 0.5, y = 0,
+    vjust = -0.2,
+    angle = 0,
+    size = 13
+  )
+
+ggsave(
+  filename = "./figs/deforestation_cumulative.png",
+  plot = viz_cumulative,
+  width = 15,
+  height = 15,
+  units = "cm"
 )
 
+save(viz_cumulative, file = "./figs/deforestation_cumulative.rdata")
+
 ## Spatial distribution ----
-(
-  map_plot <- defo %>%
-    drop_na() %>%
-    summarise(
-      deforestation_area = sum(deforestation_area),
-      .by = c("cell_id", "year")
-    ) %>%
-    arrange(cell_id, year) %>%
-    mutate(deforestation_area = deforestation_area * 0.0001) %>%
-    filter(year %in% c(2020, 2021)) %>%
-    left_join(
-      base_grid,
-      by = join_by(cell_id)
-    ) %>%
-    ggplot() +
-    facet_wrap(facets = vars(year)) +
-    geom_sf(
-      data = aoi,
-      fill = "transparent"
-    ) +
-    geom_sf(
-      aes(
-        geometry = geometry,
-        fill = deforestation_area
-      )
-    ) +
-    scale_fill_scico(
-      palette = "bilbao",
-      labels = label_number(scale_cut = cut_short_scale())
-    ) +
-    labs(
-      title = "Spatial distribution of Conservation Units",
-      fill = "Area (ha)"
-    ) +
-    theme_void() +
-    theme(text = element_text(size = 12))
+viz <- defo |>
+  filter(year %in% c(2020)) |>
+  st_as_sf() |>
+  summarise(
+    deforestation_area = sum(deforestation_area),
+    .by = c("cell_id", "year", "geometry")
+  ) |>
+  filter(deforestation_area > 800) |>
+  eda_spatial_distribution(
+    variable = deforestation_area,
+    base_map = aoi
+  ) +
+  labs(fill = "Deforestation Area (ha)") +
+  facet_wrap(facets = vars(year)) +
+  theme(
+    strip.text = element_text(),
+    legend.position = "bottom",
+    legend.key.height = unit(2, 'mm'),
+    legend.key.width = unit(25, 'mm'),
+    legend.justification = c(0.5, 0.5),
+    plot.margin = unit(c(0, 0, 0, 0), "cm")
+  ) +
+  guides(fill = guide_colourbar(title.position = "top", title.hjust = 0.5))
+
+title <- ggdraw() +
+  draw_label(
+    "Spatial Distribution of Deforestation",
+    fontface = 'bold',
+    x = 0,
+    hjust = 0
+  ) +
+  theme(
+    plot.margin = margin(0, 0, 0, 0)
+  )
+
+viz_spatial_distribution <-
+  plot_grid(
+    title,
+    viz,
+    ncol = 1,
+    rel_heights = c(0.1, 1),
+    scale = 0.9
+  )
+
+ggsave(
+  filename = "./figs/deforestation_spatial_distribution.png",
+  plot = viz_spatial_distribution,
+  width = 15,
+  height = 15,
+  units = "cm"
+)
+
+save(
+  viz_spatial_distribution,
+  file = "./figs/deforestation_spatial_distribution.rdata"
 )
 
 ## Create Time Series Plot ----
 
-defo %>%
-  left_join(
-    base_grid,
-    by = join_by(cell_id)
-  ) %>%
-  as_tibble() %>%
-  mutate(
-    date = ymd(year, truncated = 2),
-    deforestation_area = deforestation_area * 0.0001
-  ) %>%
-  summarise(
-    deforestation_area = sum(deforestation_area),
-    .by = c("date", "region_name")
-  ) %>%
-  ggplot() +
-  facet_wrap(facets = "region_name") +
-  geom_col(
-    aes(
-      x = date,
-      y = deforestation_area,
-      fill = deforestation_area
-    )
-  ) +
-  scale_fill_scico(
-    palette = "bilbao",
-    labels = label_number(),
-    trans = "log10"
-  ) +
-  theme_minimal() +
-  theme(
-    text = element_text(size = 15)
-  )
+
+
+## Create sum plot ----
+
+
 
 # SAVE PLOTS ------------------------------------------------------------------
 
@@ -265,7 +260,7 @@ ggsave(
   plot = hist_plot,
   device = ragg::agg_png,
   width = 15,
-  height = 7,
+  height = 10,
   units = "cm",
   dpi = 300
 )
