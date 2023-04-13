23832c1767a03273584d29f0bd77a3990633c982
hugotseixas <seixas.hugo@protonmail.com>
2023-04-13 17:33:22 -0300

    Create plots for conservation units.

--- analysis/eda_conservation_units.R
+++ analysis/eda_conservation_units.R
@@ -29,232 +29,174 @@ conflicts_prefer(dplyr::filter)
 
 aoi <- read_sf("data/external/aoi/aoi.fgb")
 
+aoi <-
+  smoothr::smooth(
+    st_simplify(aoi, dTolerance = 5000),
+    method = "ksmooth",
+    smoothness = 3
+  )
+
 base_grid <- read_sf("data/base_grid.fgb")
 
-uc <- read_parquet(file = "data/conservation_units.parquet")
+uc <-
+  read_parquet(file = "data/conservation_units.parquet") |>
+  left_join(
+    base_grid,
+    by = join_by(cell_id)
+  ) |>
+  select(!c(geometry, cell_area)) |>
+  mutate(uc_area = uc_area * 0.0001) |>
+  arrange(cell_id, year) |>
+  summarise(
+    uc_area = sum(uc_area),
+    .by = c("cell_id", "region_name", "year")
+  ) |>
+  mutate(
+    cumulative_uc_area = cumsum(uc_area),
+    .by = "cell_id"
+  ) |>
+  filter(year > 1986, year < 2021)
 
 # EXPLORE DATA ----------------------------------------------------------------
 
-## Time Series ----
-timeseries_plot <- uc %>%
+create_visualizations(
+  f = eda_histogram,
+  data = filter(uc, uc_area != 0),
+  variable = uc_area,
+  group_variable = region_name,
+  group_facet = TRUE,
+  viz_title = "Conservation Unit Histogram",
+  x_title = "Conservation Unit Area (ha)",
+  y_title = "",
+  scale_transform = "identity",
+  n_bins = 50,
+  x_lim = NULL,
+  out_filename = "uc_histogram",
+  out_path = "figs/eda/"
+)
+
+create_visualizations(
+  f = eda_cumulative_distribution,
+  data = filter(uc, uc_area != 0),
+  variable = uc_area,
+  group_variable = region_name,
+  group_facet = TRUE,
+  viz_title = "Cumulative Conservation Unit Area",
+  x_title = "Conservation unit Area (ha)",
+  y_title = "Cumulative Percentage (%)",
+  quantiles_list = c(0.05, 0.5, 0.75, 1),
+  scale_transform = "identity",
+  out_filename = "uc_cumulative",
+  out_path = "figs/eda/"
+)
+
+viz_data <- uc |>
+  summarise(
+    uc_area = sum(uc_area),
+    .by = c("cell_id")
+  ) |>
+  filter(uc_area != 0) |>
   left_join(
     base_grid,
     by = join_by(cell_id)
-  ) %>%
-  select(!geometry) %>%
+  ) |>
+  st_as_sf() |>
+  select(uc_area)
+
+create_visualizations(
+  f = eda_spatial_distribution,
+  data = viz_data,
+  variable = uc_area,
+  variable_label = "Conservation Unit Area (ha)",
+  group_variable = NULL,
+  viz_title = "Spatial Distribution of Conservation Units",
+  x_title = "",
+  y_title = "",
+  base_map = aoi,
+  out_filename = "uc_spatial",
+  out_path = "figs/eda/",
+  out_width = 15,
+  out_height = 15
+)
+
+viz_data <- read_parquet(file = "data/conservation_units.parquet") |>
+  filter(year > 1986, year < 2021) |>
+  left_join(
+    base_grid,
+    by = join_by(cell_id)
+  ) |>
+  select(!c(geometry, cell_area)) |>
+  drop_na() |>
+  mutate(uc_area = uc_area * 0.0001) |>
   summarise(
-    uc_area = as.numeric(sum(uc_area)),
-    .by = c("year", "region_name")
-  ) %>%
-  arrange(year) %>%
-  mutate(
-    uc_area = cumsum(uc_area),
-    uc_area = uc_area * 0.0001,
-    .by = "region_name"
-  ) %>%
-  ggplot(
-    aes(
-      x = year,
-      y = uc_area,
-      shape = region_name,
-      color = uc_area
-    )
-  ) +
-  facet_wrap(
-    facets = vars(region_name),
-    ncol = 1,
-    scales = "free_y"
-  ) +
-  geom_step() +
-  geom_point(size = 2) +
-  scale_y_continuous(
-    labels = label_number(scale_cut = cut_short_scale())
-  ) +
-  scale_x_continuous(
-    labels = label_date(format = "%Y")
-  ) +
-  scale_color_scico(
-    palette = "bilbao",
-    begin = 0.6,
-    labels = label_number(scale_cut = cut_short_scale())
-  ) +
-  labs(
-    title = "Conservation Units area time series",
-    y = "Area (ha)",
-    x = "Year"
-  ) +
-  theme_minimal() +
-  theme(
-    text = element_text(size = 12),
-    legend.position = ""
+    uc_area = sum(uc_area),
+    .by = c("region_name", "government_level")
   )
 
-## Spatial distribution ----
-map_plot <- uc %>%
+create_visualizations(
+  f = eda_colsum,
+  data = viz_data,
+  variable = uc_area,
+  group_facet = TRUE,
+  group_variable = region_name,
+  cat_variable = government_level,
+  scale_transform = "log",
+  viz_title = "Deforested Areas Classes",
+  x_title = "LULC",
+  y_title = "Deforestation Area (ha)",
+  out_filename = "uc_colsum",
+  out_path = "figs/eda/"
+)
+
+viz_data <- uc |>
   summarise(
-    uc_area = as.numeric(sum(uc_area)),
-    .by = c("cell_id", "year")
-  ) %>%
-  arrange(cell_id, year) %>%
-  mutate(
-    uc_area = cumsum(uc_area),
-    uc_area = uc_area * 0.0001,
-    .by = "cell_id"
-  ) %>%
-  filter(year %in% c(1985, 2021)) %>%
+    cumulative_uc_area = sum(cumulative_uc_area),
+    .by = c("region_name", "year")
+  ) |>
+  rename(date = year)
+
+create_visualizations(
+  f = eda_time_series,
+  data = viz_data,
+  variable = cumulative_uc_area,
+  group_facet = TRUE,
+  group_variable = region_name,
+  viz_title = "Deforestation Time Series",
+  x_title = "Year",
+  y_title = "Deforestation Area (ha)",
+  ts_type = "step",
+  out_filename = "uc_timeseries",
+  out_path = "figs/eda/"
+)
+
+viz_data <- read_parquet(file = "data/conservation_units.parquet") |>
+  filter(year > 1986, year < 2021) |>
   left_join(
     base_grid,
     by = join_by(cell_id)
-  ) %>%
-  ggplot() +
-  facet_wrap(facets = vars(year)) +
-  geom_sf(
-    data = aoi,
-    fill = "transparent"
-  ) +
-  geom_sf(
-    aes(
-      geometry = geometry,
-      fill = uc_area
-    )
-  ) +
-  scale_fill_scico(
-    palette = "bilbao",
-    labels = label_number(scale_cut = cut_short_scale())
-  ) +
-  labs(
-    title = "Spatial distribution of Conservation Units",
-    fill = "Area (ha)"
-  ) +
-  theme_void() +
-  theme(text = element_text(size = 12))
+  ) |>
+  select(!c(geometry, cell_area)) |>
+  drop_na() |>
+  mutate(uc_area = uc_area * 0.0001) |>
+  summarise(
+    uc_area = sum(uc_area),
+    .by = c("region_name", "group")
+  ) |>
+  rename(uc_type = group)
 
-## Histogram ----
-(
-  hist_plot <- uc %>%
-    summarise(
-      uc_area = as.numeric(sum(uc_area)),
-      .by = c("cell_id", "year")
-    ) %>%
-    arrange(cell_id, year) %>%
-    mutate(
-      uc_area = cumsum(uc_area),
-      uc_area = uc_area * 0.0001,
-      .by = "cell_id"
-    ) %>%
-    left_join(
-      base_grid,
-      by = join_by(cell_id)
-    ) %>%
-    select(!geometry) %>%
-    mutate(uc_area = as.numeric(uc_area)) %>%
-    filter(uc_area > 0) %>%
-    ggplot() +
-    facet_wrap(
-      facets = vars(region_name),
-      ncol = 1
-    ) +
-    geom_histogram(
-      aes(x = uc_area, y = after_stat(count)),
-      color = "#000000",
-      fill = "#e8e8e8",
-      bins = 50
-    ) +
-    scale_x_log10(
-      breaks = log_breaks(6),
-      labels = label_number(accuracy = 1),
-      expand = c(0, 0)
-    ) +
-    labs(
-      title = "Conservation Units delimitation histogram",
-      x = "Area (ha)"
-    ) +
-    theme_minimal() +
-    theme(
-      text = element_text(size = 12),
-      axis.text.y = element_blank(),
-      axis.title.y = element_blank(),
-      panel.grid.major.y = element_blank(),
-      panel.grid.minor.y = element_blank()
-    )
+create_visualizations(
+  f = eda_colsum,
+  data = viz_data,
+  variable = uc_area,
+  group_facet = TRUE,
+  group_variable = region_name,
+  cat_variable = uc_type,
+  scale_transform = "identity",
+  viz_title = "Conservation Units Classes",
+  x_title = "Conservation Unit Class",
+  y_title = "Conservation Unit Area (ha)",
+  out_filename = "uc_colsum",
+  out_path = "figs/eda/"
 )
 
 
-## Columns sum ----
-(
-  colsum_plot <- uc %>%
-    left_join(
-      base_grid,
-      by = join_by(cell_id)
-    ) %>%
-    select(!geometry) %>%
-    drop_na(group) %>%
-    summarise(
-      uc_area = sum(uc_area),
-      .by = c("group", "region_name")
-    ) %>%
-    mutate(uc_area = as.numeric(uc_area) * 0.0001) %>%
-    ggplot() +
-    facet_wrap(facets = "region_name") +
-    geom_col(
-      aes(x = group, y = uc_area, group = group),
-      color = "#000000",
-      fill = "#e8e8e8"
-    ) +
-    scale_y_continuous(
-      labels = label_number(scale_cut = cut_short_scale())
-    ) +
-    labs(
-      title = "Conservation Units area by type",
-      x = "Conservation Unit Type",
-      y = "Area (ha)"
-    ) +
-    theme_minimal() +
-    theme(text = element_text(size = 12))
-)
-
-# SAVE PLOTS ------------------------------------------------------------------
-
-# Save time series plot
-ggsave(
-  filename = "./figs/conservation_units_timeseries.png",
-  plot = timeseries_plot,
-  device = ragg::agg_png,
-  width = 15,
-  height = 8,
-  units = "cm",
-  dpi = 300
-)
-
-# Save map plot
-ggsave(
-  filename = "./figs/conservation_units_map.png",
-  plot = map_plot,
-  device = ragg::agg_png,
-  width = 15,
-  height = 8,
-  units = "cm",
-  dpi = 300
-)
-
-# Save colsum plot
-ggsave(
-  filename = "./figs/conservation_units_colsum.png",
-  plot = colsum_plot,
-  device = ragg::agg_png,
-  width = 15,
-  height = 8,
-  units = "cm",
-  dpi = 300
-)
-
-# Save histogram plot
-ggsave(
-  filename = "./figs/conservation_units_hist.png",
-  plot = hist_plot,
-  device = ragg::agg_png,
-  width = 15,
-  height = 8,
-  units = "cm",
-  dpi = 300
-)
