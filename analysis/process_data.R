9ce07ac9eca0981a98a3f376bed78bd5ebdf3809
hugotseixas <seixas.hugo@protonmail.com>
2023-08-15 19:12:02 -0300

    Add script to run all data processing functions.

---
+++ analysis/process_data.R
@@ -0,0 +1,45 @@
+# HEADER ----------------------------------------------------------------------
+#
+# Title:          Process Data
+# Description:    This process data to fit the base grid
+#
+# Notes:
+#
+# LIBRARIES -------------------------------------------------------------------
+#
+library(tica)
+library(tidyverse)
+#
+# CONFLICTS -------------------------------------------------------------------
+#
+
+#
+# OPTIONS ---------------------------------------------------------------------
+#
+
+#
+# PROCESS DATA ----------------------------------------------------------------
+
+arguments <- read_delim("./analysis/process_data_arguments.txt", delim = "|")
+
+pmap(
+  .l = arguments,
+  .f = \(
+    f,
+    base_grid_path,
+    external_data_path,
+    dest_dir,
+    start_year,
+    end_year
+  ) {
+
+    process_external_data(
+      f = f,
+      base_grid_path = base_grid_path,
+      external_data_path = external_data_path,
+      dest_dir = dest_dir,
+      timespan = start_year:end_year
+    )
+
+  }
+)
